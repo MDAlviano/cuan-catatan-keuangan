56b95a72f9095f5daacd2b2d780e3960d7d093a0
MDAlviano <mdalviano.21122@gmail.com>
2025-02-02 18:54:16 +0700

    add component: CustomButton.kt, OrLine.kt, RedirectText.kt, Theme.kt, TopBar.kt

--- app/src/main/java/com/cuan/catatankeuangan/presentation/components/OrLine.kt
+++ app/src/main/java/com/cuan/catatankeuangan/presentation/components/OrLine.kt
@@ -1,14 +1,11 @@
 package com.cuan.catatankeuangan.presentation.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
--- app/src/main/java/com/cuan/catatankeuangan/presentation/components/TopBar.kt
+++ app/src/main/java/com/cuan/catatankeuangan/presentation/components/TopBar.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
